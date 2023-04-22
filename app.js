a5760fcd819c49cf90ea4e6d7ac76a6774dd25be
cho <aifee0518@gmail.com>
2023-04-22 18:13:14 +0900

    create app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('express 띄우기 성공!');
+});
+
+app.listen(port, () => {
+  console.log(`짜자잔!. http://localhost:${port}`);
+});
